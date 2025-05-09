cebed350976042a7b6b2a92c767cfc46ee86581d
justman0898 <jayigboneme@gmail.com>
2025-05-09 00:30:56 -0700

    First push

---
+++ count_down.py
@@ -0,0 +1,11 @@
+integer = int(input("Enter an integer: "))
+
+while integer < 1 :
+	integer = int(input("Enter an integer: "))
+
+
+for integer in range (integer, 0, -1) :
+	if integer == 1:
+		print("BLAST OFF!!")
+	else: 
+		print(integer)
